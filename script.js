f07f021602cc5ac1c843a2241c535e81784a14a9
Vivek Aher <vivek4879gmail.com>
2026-02-11 13:31:31 -0500

    feat: Add password length input and generate two passwords displayed in input fields with updated styling.

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+
+
+let password_length = document.getElementById("password-length");
+let password_box1 = document.getElementById("password-box1");
+let password_box2 = document.getElementById("password-box2");
+let gen_button = document.getElementById("gen-button");
+
+
+
+function generate_password(){
+    if(password_length.value === ""){
+        return;
+    }
+    if(password_length.value > 15)return;
+    let password = "";
+    for(let i = 0; i < password_length.value; i++){
+        let random_index = Math.floor(Math.random() * characters.length);
+        password+=characters[random_index];
+    }
+    return password;
+}
+
+
+function render_password(){
+    password_box1.value = generate_password();
+    password_box2.value = generate_password();
+}
